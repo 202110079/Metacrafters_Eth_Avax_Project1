e77f6fa445f1be1c1ac5a8bbd92ba3e43602edae
202110079 <202110079@fit.edu.ph>
2024-11-18 16:02:31 +0800

    Changed according to Metamask recommendation

--- eth_avax_project1.sol
+++ eth_avax_project1.sol
@@ -1,30 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-/*
-       REQUIREMENTS
-Contract successfully uses require()
-Contract successfully uses assert()
-Contract successfully uses revert() statements
-*/
-
 contract project1 {
-    //require
-    function requireFunc(int _number) public pure returns (int) {
-        require(_number > 5, "The number must be higher than 5."); 
-        return _number += 1;
+    address public owner;
+    int256 public testBal;
+
+    // Constructor to set the owner of the contract
+    constructor() {
+        owner = msg.sender;
+        testBal = 100;
     }
 
-    //assert
-    function assertFunc(int _number) public pure returns (bool){
-        assert(_number > 10); 
-        return false;
+    // require: Ensures the sender is the owner before adding +1 to the number
+    function requireFunc(int _number) public returns (int) {
+        require(msg.sender == owner, "Caller is not the owner."); 
+        testBal += _number;
+        return testBal;
     }
 
-    //revert
-    function revertFunc(int _number) public pure {
-        if (_number <= 10){
-            revert("The number is not high enough!");
+    // assert
+    function assertFunc(int _number) public view returns (bool) {
+        assert(_number > testBal); 
+        return true;
+    }
+
+    // revert
+    function revertFunc(int _number) public view {
+        if (_number <= testBal) {
+            revert("The number is not higher than the balance!");
         }
+    }
+    
+    // Function to return the contract's balance
+    function getBalance() public view returns (int256) {
+        return testBal;
     }
 }
